41a8e93cf20577edfd625a023f6f87b9b98006f6
hfurkanvural <hfurkanvural@gmail.com>
2020-05-17 20:55:23 +0300

    API Gateway added.

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from aws_cdk import core
 
 from todo_lambda.todo_lambda_stack import TodoLambdaStack
 
+environment = core.Environment(region="eu-central-1")
 
 app = core.App()
 TodoLambdaStack(app, "todo-lambda")
--- todo_lambda/todo_lambda_stack.py
+++ todo_lambda/todo_lambda_stack.py
@@ -1,9 +1,23 @@
-from aws_cdk import core
-
-
+from aws_cdk import (
+    core,
+    aws_lambda as _lambda,
+    aws_apigateway as api_gateway
+)
 class TodoLambdaStack(core.Stack):
 
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
+        lambda_greetings = _lambda.Function(
+            self, 'GreetingsHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.asset('lambda'),
+            handler='greetings.greet',
+            memory_size=128
+        )
+        api_stage_options = api_gateway.StageOptions(stage_name="dev")
+        api = api_gateway.LambdaRestApi(self, 'Endpoint', handler=lambda_greetings, deploy_options=api_stage_options)
+        
 
-        # The code that defines your stack goes here
+
+
+
